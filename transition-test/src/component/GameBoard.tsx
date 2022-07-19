936d652a24ecbe1248234d09f2e56ca3382bf5ea
fifteenmania <ktthee1995@gmail.com>
2022-07-19 19:54:31 +0900

    sync

--- transition-test/src/component/GameBoard.tsx
+++ transition-test/src/component/GameBoard.tsx
@@ -1,3 +1,6 @@
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+
 function TestBox() {
   return <div className="w-10 h-10 bg-gray-900 opacity-80">
     10
@@ -13,7 +16,11 @@ function Stack() {
 
 export default function GameBoard() {
   const numBox = 10;
-  return <div>
-    <Stack />
+  const boxRef = useRef(null)
+  useEffect(() => {
+    gsap.to(boxRef.current, {rotation: "+=360"})
+  })
+  return <div className=" w-32 h-32" ref={boxRef}>
+    hello
   </div>
 }
