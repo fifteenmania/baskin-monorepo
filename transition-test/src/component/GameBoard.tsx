d856d3582e65fc50c069fb3f1d4f27cbb8e2c311
fifteenmania <ktthee1995@gmail.com>
2022-07-20 19:20:08 +0900

    feat: transition-box add test

--- transition-test/src/component/GameBoard.tsx
+++ transition-test/src/component/GameBoard.tsx
@@ -1,26 +1,58 @@
-import gsap from "gsap";
-import { useEffect, useRef } from "react";
+import gsap from "gsap"
+import { forwardRef, MutableRefObject, ReactNode, useEffect, useRef, useState } from "react";
 
-function TestBox() {
-  return <div className="w-10 h-10 bg-gray-900 opacity-80">
-    10
-  </div>
+interface BoxProps {
+  key: number,
+  displayNum: number,
+  callback: () => void
 }
 
-function Stack() {
-  const maxLen = 8;
+function DisappearingBox({displayNum, callback} : BoxProps) { 
+  const el = useRef(null)
+  const tl = useRef<ReturnType<typeof gsap.timeline> | null>(null)
+  useEffect(() => {
+    tl.current = gsap.timeline()
+        .to(el.current, {x: 100, opacity: 0, duration: 1})
+        .call(callback)
+  }, [callback])
+  return (
+    <div className="disappearing-box" ref={el}>
+      {displayNum}
+    </div>
+  )
+}
+
+
+function BoxQueue() {
+  const [queue, setQueue] = useState<BoxProps[]>([]);
+  const [count, setCount] = useState(0);
+  const addBox = (key: number) => {
+    const newBox = {
+      key,
+      displayNum: count,
+      callback: () => removeBox(key)
+    }
+    setQueue(queue => [...queue, newBox])
+  }
+  const removeBox = (key: number) => {
+    setQueue(queue => queue.filter(boxProps => boxProps.key !== key))
+  }
+  const onButtonClick = () => {
+    addBox(count)
+    setCount((count) => count + 1)
+  }
   return <div className="flex flex-col">
-    {Array.from({length: maxLen}).map((_, idx) => <TestBox key={idx}/>)}
+    <button onClick={onButtonClick}>click me!</button>
+    {queue.map((boxProps, index) => (
+      <DisappearingBox key={boxProps.key} displayNum={boxProps.displayNum} callback={boxProps.callback} />
+    ))}
   </div>
 }
+
+
 
 export default function GameBoard() {
-  const numBox = 10;
-  const boxRef = useRef(null)
-  useEffect(() => {
-    gsap.to(boxRef.current, {rotation: "+=360"})
-  })
-  return <div className=" w-32 h-32" ref={boxRef}>
-    hello
+  return <div className="w-16 h-16 text-2xl bg-slate-500">
+    <BoxQueue />
   </div>
 }
--- transition-test/tailwind.config.js
+++ transition-test/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js,ts,jsx,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 }
